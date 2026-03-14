77cacf85c6099069d92c4cd78640c2ae07367271
itesh-singh <itesh5906@gmail.com>
2026-03-14 23:22:56 +0530

    chore: remove superuser creation from build script

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from accounts.models import User; User.objects.create_superuser('itesh', 'itesh@admin.com', 'Admin@1234') if not User.objects.filter(username='itesh').exists() else print('exists')" | python manage.py shell
